3db00c67888cea5a73a9663a66a66fbc63c59716
QuestofGalia <morality666@gmail.com>
2014-09-19 20:27:42 +0300

    Quyqquluuruu
    Testattu aukeaminen

--- Unity/Assets/Hello.cs
+++ Unity/Assets/Hello.cs
@@ -6,6 +6,7 @@ public class Hello : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.Log("En voi hyvin");
+		//Quqquluuruu saatana t marko
 	}
 	
 	// Update is called once per frame
