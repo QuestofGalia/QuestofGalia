d9f12e019faad9989fc061fa9330f9d8bec40bba
Jeremias Isohanni <jeremias.isohanni@gmail.com>
2014-09-23 22:10:47 +0300

    Päivitystä

--- Unity/Assets/Hello.cs
+++ Unity/Assets/Hello.cs
@@ -6,7 +6,6 @@ public class Hello : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.Log("En voi hyvin");
-		//Quqquluuruu saatana t marko
 	}
 	
 	// Update is called once per frame
