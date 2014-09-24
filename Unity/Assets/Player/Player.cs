f20ea3216a00d9990e2e746ee1ba809cb127f1b3
Jeremias Isohanni <jeremias.isohanni@gmail.com>
2014-09-24 13:28:11 +0300

    Hahmottelua v2
    Liikkumista

---
+++ Unity/Assets/Player/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    
+
+    public float speed;
+    private Vector3 movement;
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+        movement = Vector3.zero;
+
+        if(Input.GetKey(KeyCode.W)) {
+            movement.z += speed;
+        }
+
+        if(Input.GetKey(KeyCode.S)) {
+            movement.z -= speed;
+        }
+
+        if(Input.GetKey(KeyCode.D)) {
+            movement.x += speed;
+        }
+
+        if(Input.GetKey(KeyCode.A)) {
+            movement.x -= speed;
+        }
+
+        transform.Translate(movement * Time.deltaTime);
+	}
+}
