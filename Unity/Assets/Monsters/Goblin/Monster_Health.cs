245d6a81619fa48adba1da2c3c47ad2c920651ce
Jeremias Isohanni <jeremias.isohanni@gmail.com>
2014-10-17 16:11:13 +0300

    Player & Monster health + death.
    Player & Monster health + death.

---
+++ Unity/Assets/Monsters/Goblin/Monster_Health.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster_Health : MonoBehaviour {
+
+    // Use this for initialization
+    public float health = 10;
+    public float resetAfterDeathTime = 1.5f;
+
+    private Monsterai monsterMovement;
+    private float timer;
+    private bool monsterDead;
+
+
+    void Awake()
+    {
+        monsterMovement = GetComponent<Monsterai>();
+    }
+
+
+    void Update()
+    {
+
+        if (health <= 0f)
+        {
+            if (!monsterDead)
+            {
+                monsterDying();
+            }
+            else
+            {
+                MonsterDead();
+            }
+
+        }
+    }
+
+    void monsterDying()
+    {
+        monsterDead = true;
+    }
+
+    void MonsterDead()
+    {
+        monsterMovement.enabled = false;
+        timer += Time.deltaTime;
+
+        if (timer >= resetAfterDeathTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Unity/Assets/Player/Player_Health.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Health : MonoBehaviour {
+
+	// Use this for initialization
+    public float health = 50;
+    public float resetAfterDeathTime = 5;
+
+    private Player playerMovement;
+    private float timer;
+    private bool playerDead;
+
+
+    void Awake()
+    {
+        playerMovement = GetComponent<Player>();
+
+    }
+	
+
+	void Update () {
+
+        if (health <= 0f)
+        {
+            if (!playerDead)
+            {
+                PlayerDying();
+            }
+            else
+            {
+                PlayerDead();
+                LevelReset();
+            }
+                
+        }
+	}
+
+    void PlayerDying()
+    {
+        playerDead = true;
+    }
+
+    void PlayerDead()
+    {
+        playerMovement.enabled = false;
+    }
+
+    void LevelReset()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= resetAfterDeathTime)
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+    }
+}
