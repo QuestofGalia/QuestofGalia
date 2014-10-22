627fd50d868ab90cbf0e7b642ad5737aad54d18e
Jeremias Isohanni <jeremias.isohanni@gmail.com>
2014-10-22 14:06:37 +0300

    Level 1 updatea v2
    Kaikkea paskaa

---
+++ Unity/Assets/Level changer/level_change.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class level_change : MonoBehaviour {
+
+    public int nextLevel;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Application.LoadLevel(nextLevel);
+        }
+    }
+}
