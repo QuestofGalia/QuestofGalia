4e8fef36fd68f0a3355fd1fb508d6495d7ee4a75
QuestofGalia <morality666@gmail.com>
2014-10-29 14:29:15 +0200

    Ovet
    Ovet lisätty level 1 ja ne mitkä ei vaadi avainta pelittää ny

---
+++ Unity/Assets/Doors/openovi.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class openovi : MonoBehaviour {
+
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			Destroy(gameObject);
+		}
+	}
+
+
+}
